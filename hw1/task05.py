a906e64e18a40ab966eb0db51a8dcbb9da4c6089
Evgenii Prosvirnin <proevgenii19@gmail.com>
2021-03-07 23:26:39 +0300

    made changes(del reqursion)

--- hw1/task05.py
+++ hw1/task05.py
@@ -11,22 +11,22 @@ Examples:
 """
 from typing import List
 
-nums = [1, 3, -1, -3, 5, 3, 6, 7]
 result = []
 
 
 def find_maximal_subarray_sum(nums: List[int], k: int) -> int:
-    assert len(nums) > k
-    summ = 0
-    for i in range(k):
-        summ += nums[i]
+    if len(nums) < k:
+        return 'Ivalid "k" value '
+    while k > 1:
+        summ = 0
+        for i in range(k):
+            summ += nums[i]
 
-    sum_iter = summ
-    for j in range(k, len(nums)):
-        sum_iter += nums[j] - nums[j - k]
-        summ = max(summ, sum_iter)
-        result.append(max(summ, sum_iter))
-    if k > 1:
-        find_maximal_subarray_sum(nums, k - 1)
+        sum_iter = summ
+        for j in range(k, len(nums)):
+            sum_iter += nums[j] - nums[j - k]
+            summ = max(summ, sum_iter)
+            result.append(max(summ, sum_iter))
 
+        k = k - 1
     return max(result)
